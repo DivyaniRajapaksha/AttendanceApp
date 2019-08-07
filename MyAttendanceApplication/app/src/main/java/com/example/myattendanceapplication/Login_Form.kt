084779bd4500e22ba6322cc1e8faef269243eacf
divyani123 <divyanirajapaksha@gmail.com>
2019-08-07 19:25:29 +0530

    Login and Registration of Teachers

---
+++ MyAttendanceApplication/app/src/main/java/com/example/myattendanceapplication/Login_Form.kt
@@ -0,0 +1,12 @@
+package com.example.myattendanceapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_Form : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login__form)
+    }
+}
